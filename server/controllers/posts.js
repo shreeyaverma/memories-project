f8dfa197beac1d7ff9425dbc16e1d554d23bbe75
shreeyaverma <shreeyaverma80@gmail.com>
2024-08-12 19:26:28 +0530

    update, like and delete post functionality added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 
 export const getPost = async (req, res) => {
@@ -21,4 +22,39 @@ export const createPost = async (req, res) => {
     catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+//  /posts/123
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send('No post with that ID');
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send('No post with that ID')
+    }
+
+    console.log("DELETE!")
+    await PostMessage.findByIdAndDelete(id);
+    res.json({ message: "Post deleted successfully" });
+};
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send('No post with that ID')
+    }
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    res.json(updatedPost);
 }
--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -17,7 +19,7 @@ app.use('/posts', postRoutes)
 const CONNECTION_URL = 'mongodb+srv://shreeyaverma:Karmarule$123@cluster0.xfqbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`server running on PORT: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
